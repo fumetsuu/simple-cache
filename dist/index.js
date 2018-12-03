8dfbb4883f54c61fc07b9cd0fea1ecf592fcf17d
Joseph Surin <joseph.surin@gmail.com>
2018-12-03 12:18:08 +0800

    simple cache basic functionality

---
+++ dist/index.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var md5 = require('md5');
+var fs = require('fs');
+var path = require('path');
+
+var getHashedFilename = function getHashedFilename(key) {
+	return md5(key) + '.cache';
+};
+
+function simpleCache() {
+	var _this = this;
+
+	this.dirPath = '';
+
+	/**
+  * initialise the simpleCache instance
+  * @param {string} dpath - path to the cache directory
+  * @return {simpleCache} this instance
+  */
+	this.init = function (dpath) {
+		_this.dirPath = dpath;
+		if (!fs.existsSync(dpath)) {
+			fs.mkdirSync(dpath);
+		}
+		return _this;
+	};
+
+	/**
+  * sets a key-value pair in a file in the cache directory
+  * @param {string} key - Unique identifier
+  * @param {dynamic} value - JSON serializable data
+  * @return {Promise} resolves into object containing cached file info
+  */
+	this.set = function (key, value) {
+		return new Promise(function (resolve, reject) {
+			var hashFilename = getHashedFilename(key);
+			fs.writeFile(path.join(_this.dirPath, hashFilename), JSON.stringify(value), function (err) {
+				if (err) reject(err);
+				var cachedFile = {
+					key: key,
+					hashFilename: hashFilename,
+					data: value
+				};
+				resolve(cachedFile);
+			});
+		});
+	};
+
+	/**
+  * gets the value given a key by looking up for the corresponding file in the cache directory
+  * @param {string} key - Unique identifier
+  * @return {Promise} resolves into object containing cached file info
+  */
+	this.get = function (key) {
+		return new Promise(function (resolve, reject) {
+			var hashFilename = getHashedFilename(key);
+			fs.readFile(path.join(_this.dirPath, hashFilename), function (err, data) {
+				if (err) return reject(err);
+				var cachedFile = {
+					key: key,
+					hashFilename: hashFilename,
+					data: JSON.parse(data.toString())
+				};
+				resolve(cachedFile);
+			});
+		});
+	};
+
+	/**
+  * asynchronously checks if the file with the given key is cached in the cache directory
+  * @param {string} key - Unique identifier
+  * @return {Promise} boolean
+  */
+	this.isCached = function (key) {
+		return new Promise(function (resolve) {
+			var hashFilename = getHashedFilename(key);
+			fs.access(path.join(_this.dirPath, hashFilename), fs.constants.F_OK, function (err) {
+				resolve(!err);
+			});
+		});
+	};
+
+	/**
+  * synchronous version of isCached
+  * @param {string} key - Unique identifier
+  * @return boolean
+  */
+	this.isCachedSync = function (key) {
+		var hashFilename = getHashedFilename(key);
+		return fs.existsSync(path.join(_this.dirPath, hashFilename));
+	};
+
+	/**
+  * deletes the file associated with the given key
+  * @param {string} key - Unique identifier
+  * @return {Promise} resolves to true if successful
+  */
+	this.delete = function (key) {
+		return new Promise(function (resolve, reject) {
+			var hashFilename = getHashedFilename(key);
+			fs.unlink(path.join(_this.dirPath, hashFilename), function (err) {
+				if (err) return reject(err);
+				resolve(true);
+			});
+		});
+	};
+
+	/**
+  * synchronous version of delete
+  * @param {string} key - Unique identifier
+  * @return {void}
+  */
+	this.deleteSync = function (key) {
+		var hashFilename = getHashedFilename(key);
+		fs.unlinkSync(hashFilename);
+	};
+
+	/**
+  * asynchronously deletes cache directory
+  * @return {Promise} resolves to true if successful
+  */
+	this.deleteAll = function () {
+		return Promise(function (resolve, reject) {
+			fs.rmdir(_this.dirPath, function (err) {
+				if (err) return reject(err);
+				resolve(true);
+			});
+		});
+	};
+}
+
+module.exports = simpleCache;
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,128 @@
+const md5 = require('md5')
+const fs = require('fs')
+const path = require('path')
+
+const getHashedFilename = (key) => md5(key) + '.cache'
+
+function simpleCache() {
+	
+	this.dirPath = ''
+
+	/**
+	 * initialise the simpleCache instance
+	 * @param {string} dpath - path to the cache directory
+	 * @return {simpleCache} this instance
+	 */
+	this.init = (dpath) => {
+		this.dirPath = dpath
+		if(!fs.existsSync(dpath)) {
+			fs.mkdirSync(dpath)
+		}
+		return this
+	}
+
+	/**
+	 * sets a key-value pair in a file in the cache directory
+	 * @param {string} key - Unique identifier
+	 * @param {dynamic} value - JSON serializable data
+	 * @return {Promise} resolves into object containing cached file info
+	 */
+	this.set = (key, value) => {
+		return new Promise((resolve, reject) => {
+			var hashFilename = getHashedFilename(key)
+			fs.writeFile(path.join(this.dirPath, hashFilename), JSON.stringify(value), (err) => {
+				if(err) reject(err)
+				var cachedFile = {
+					key,
+					hashFilename,
+					data: value
+				}
+				resolve(cachedFile)
+			})
+		})
+	}
+
+	/**
+	 * gets the value given a key by looking up for the corresponding file in the cache directory
+	 * @param {string} key - Unique identifier
+	 * @return {Promise} resolves into object containing cached file info
+	 */
+	this.get = (key) => {
+		return new Promise((resolve, reject) => {
+			var hashFilename = getHashedFilename(key)
+			fs.readFile(path.join(this.dirPath, hashFilename), (err, data) => {
+				if(err) return reject(err)
+				var cachedFile = {
+					key,
+					hashFilename,
+					data: JSON.parse(data.toString())
+				}
+				resolve(cachedFile)
+			})
+		})
+	}
+
+	/**
+	 * asynchronously checks if the file with the given key is cached in the cache directory
+	 * @param {string} key - Unique identifier
+	 * @return {Promise} boolean
+	 */
+	this.isCached = (key) => {
+		return new Promise((resolve) => {
+			var hashFilename = getHashedFilename(key)
+			fs.access(path.join(this.dirPath, hashFilename), fs.constants.F_OK, (err) => {
+				resolve(!err)
+			})
+		})
+	}
+
+	/**
+	 * synchronous version of isCached
+	 * @param {string} key - Unique identifier
+	 * @return boolean
+	 */
+	this.isCachedSync = (key) => {
+		var hashFilename = getHashedFilename(key)
+		return fs.existsSync(path.join(this.dirPath, hashFilename))
+	}
+
+	/**
+	 * deletes the file associated with the given key
+	 * @param {string} key - Unique identifier
+	 * @return {Promise} resolves to true if successful
+	 */
+	this.delete = (key) => {
+		return new Promise((resolve, reject) => {
+			var hashFilename = getHashedFilename(key)
+			fs.unlink(path.join(this.dirPath, hashFilename), (err) => {
+				if(err) return reject(err)
+				resolve(true)
+			})
+		})
+	}
+
+	/**
+	 * synchronous version of delete
+	 * @param {string} key - Unique identifier
+	 * @return {void}
+	 */
+	this.deleteSync = (key) => {
+		var hashFilename = getHashedFilename(key)
+		fs.unlinkSync(hashFilename)
+	}
+
+	/**
+	 * asynchronously deletes cache directory
+	 * @return {Promise} resolves to true if successful
+	 */
+	this.deleteAll = () => {
+		return Promise((resolve, reject) => {
+			fs.rmdir(this.dirPath, (err) => {
+				if(err) return reject(err)
+				resolve(true)
+			})
+		})
+	}
+}
+
+module.exports = simpleCache
